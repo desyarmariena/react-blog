f1a3b5f8f1cc9066c5acedd5eafc392b5280803c
Desy Armariena <desy.armariena@gmail.com>
2018-09-23 11:22:41 +0700

    React Starter

---
+++ src/pages/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    // extends React Component agar lifecycle React dapat dijalankan
+    constructor(){    
+        super() //method super wajib dipakai
+
+        //inisialisasi data/state
+        this.state = {
+            loading: true
+        }
+    }
+    
+    componentDidMount() {
+        setTimeout(() => {
+            //cara mengubah state
+            this.setState( {loading: false} )
+        }, 1000)
+    }
+
+    render() {
+        return (
+            <h1>Loading : { JSON.stringify(this.state.loading) }</h1>
+        )
+    }
+}
+
+// export agar komponen bisa dipakai di tempat lain
+export default App
